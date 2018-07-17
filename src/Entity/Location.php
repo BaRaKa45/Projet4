8bb4d090d4f2a6a2980489a4247311c4b2c285ca
Anthony Sottejeau <anthony.sottejeau@gmail.com>
2018-07-17 13:53:50 +0200

    Entity User

---
+++ src/Entity/Location.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LocationRepository")
+ */
+class Location
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bedroom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bed;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bathroom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $endDate;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBedroom(): ?int
+    {
+        return $this->bedroom;
+    }
+
+    public function setBedroom(int $bedroom): self
+    {
+        $this->bedroom = $bedroom;
+
+        return $this;
+    }
+
+    public function getBathroom(): ?int
+    {
+        return $this->bathroom;
+    }
+
+    public function setBathroom(int $bathroom): self
+    {
+        $this->bathroom = $bathroom;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getBed(): ?int
+    {
+        return $this->bed;
+    }
+
+    public function setBed(int $bed): self
+    {
+        $this->bed = $bed;
+
+        return $this;
+    }
+}
